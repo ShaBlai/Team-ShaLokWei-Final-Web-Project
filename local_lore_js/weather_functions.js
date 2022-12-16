23b2619847025f99531957b684c59de9070ba5a1
Shawn Blair <shawn.blair@tlgcohort.com>
2022-12-16 10:36:57 -0800

    Add "Secrets" js to track API keys & Weather Func

--- local_lore_js/weather_functions.js
+++ local_lore_js/weather_functions.js
@@ -1,11 +1,12 @@
 //Weather Related Stuff
+import { WEATHER_API_KEY } from "./secrets";
+import { getData } from "./api_handler";
+
 export function getWeatherByLocation({
   coords: { latitude: lat, longitude: lon },
 }) {
   //figure out a way to find user's lat & lon
-  //got the api url
-
-  const apiWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=2197ac5d11462f1f2832626a64b2bfef&units=imperial`;
+  const apiWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=imperial`;
 
   fetch(apiWeatherUrl).then((res) =>
     res.json().then((json) => {
