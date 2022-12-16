71a937a00371cf3025945e7cf6db6deeee540cb1
Shawn Blair <shawn.blair@tlgcohort.com>
2022-12-16 15:23:43 -0800

    Starting News Logic and added Card for testing

--- local_lore_js/main.js
+++ local_lore_js/main.js
@@ -1,11 +1,28 @@
-import { getWeatherByLocation } from "../weather/weather.js";
-import { handleCustomError } from "../errors/error.js";
+// //import { getWeatherByLocation } from "../weather/weather.js";
+// import { handleCustomError } from "../errors/error.js";
+import { getNewsEvents } from "./news.js";
 
-/* Get user's location and weather data if we can */
-if (navigator.geolocation) {
-  // Ask the user for permission to use their location
-  navigator.geolocation.getCurrentPosition(
-    getWeatherByLocation,
-    handleCustomError
-  );
-}
+//Search form submit handler
+user_form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  getNewsEvents(location.value);
+  //TODO Commented out for troubleshooting of News Results
+  //user_form.reset();
+});
+
+// /* Get user's location and weather data if we can */
+// if (navigator.geolocation) {
+//   // Ask the user for permission to use their location
+//   navigator.geolocation.getCurrentPosition(
+//     getWeatherByLocation,
+//     handleCustomError
+//   );
+// }
+
+// function handleLocationSuccess(loc) {
+//   alert("User accepted");
+// }
+
+// function handleLocationError(err) {
+//   alert(err.message);
+// }
