4b4a2f14cbb2c04bc0910f7f79d12a542dd0db62
Chongwei Ma <chongwei.ma@outlook.com>
2022-12-20 08:12:51 -0800

    html design updated

--- local_lore_js/current_events.js
+++ local_lore_js/current_events.js
@@ -1,8 +1,10 @@
-import { GOOGLE_EVENTS_API_KEY } from "./secrets";
-import { getData } from "./api_handler";
+import { GOOGLE_EVENTS_API_KEY } from "./secrets.js";
+import { getData } from "./api_handler.js";
 
 export function getGoogleEvents(searchTerm) {
-  const apiUrl = `https://serpapi.com/search.json?engine=google_events&q=Events+in+${searchTerm}&hl=en&gl=us`;
+  const apiUrl = `https://serpapi.com/search.json?engine=google_events&q=events+in+seattle&location=Seattle%2C+Washington%2C+United+States&api_key=c0ed3897d89592a4e6c022d8318cb0fa20002e1aa6b50a5481364d62ed1a2b57`;
 
-  getData(apiUrl).then((json) => {});
+  getData(apiUrl).then((json) => {
+    console.log(json);
+  });
 }
--- local_lore_js/main.js
+++ local_lore_js/main.js
@@ -1,28 +1,30 @@
+//import { getNewsEvents } from "./news.js";
+import { getGoogleEvents } from "./current_events.js";
 
-import { handleCustomError } from "../errors/error.js";
-import { getNewsEvents } from "./news.js";
+let header_search = document.getElementById("header_search");
 
-//import { getWeatherByLocation } from "../weather/weather.js";
-//import { handleCustomError } from "./error_handling_module.js";
+header_search.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  getGoogleEvents("12345");
+
+  alert("clicked");
+});
+
 //import { loadWeatherModule } from "./weather_module.js";
-
-//handleCustomError("test");
-
 //loadWeatherModule();
 
 /* Get user's location and weather data if we can */
 
 // import { handleCustomError } from "../errors/error.js";
 
-
-//Search form submit handler
-user_form.addEventListener("submit", (event) => {
-  event.preventDefault();
-  getNewsEvents(location.value);
-  //TODO Commented out for troubleshooting of News Results
-  //user_form.reset();
-});
-
+// //Search form submit handler
+// user_form.addEventListener("submit", (event) => {
+//   event.preventDefault();
+//   getNewsEvents(location.value);
+//   //TODO Commented out for troubleshooting of News Results
+//   //user_form.reset();
+// });
 
 // function handleLocationSuccess(loc) {
 //   alert("User accepted");
