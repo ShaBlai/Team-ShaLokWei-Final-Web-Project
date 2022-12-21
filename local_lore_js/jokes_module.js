25e0edd1e0f976041f5922a521edd0621650dca6
Lok Tamang <84638841+loktama21@users.noreply.github.com>
2022-12-21 11:01:52 -0600

    Delete jokes_module.js

--- local_lore_js/jokes_module.js
+++
@@ -1,8 +0,0 @@
-import { getData } from "./api_handler.js";
-
-const jokeURL =
-  "https://api.openbrewerydb.org/breweries?by_city=san_diego&per_page=20";
-
-export const load_jokes = () => {
-  getData(jokeURL).then((json) => console.log(json));
-};
