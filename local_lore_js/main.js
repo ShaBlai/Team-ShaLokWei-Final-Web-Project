81ce6e7e4a87e4d6bcf97ae392db0dbc7b7ef85d
12TheHangedMan/TLGCohort <chongwei.ma@outlook.com>
2022-12-21 17:14:58 -0800

    againagain

--- local_lore_js/main.js
+++ local_lore_js/main.js
@@ -4,12 +4,4 @@ load_auto_fill_module();
 import { load_fetch_content_module } from "./fetch_content_module.js";
 load_fetch_content_module();
 
-//Search form submit handler
-
-// function handleLocationSuccess(loc) {
-//    alert("User accepted");
-//  }
-
-//  function handleLocationError(err) {
-//   alert(err.message);
-//  }
+//src="https://i.ibb.co/vX45rfp/Local-Lore-Banner.jpg"
